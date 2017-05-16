3cfcd8b9969f575a33780733acf736ff54efd0e9
JP BLOND <jpb.blond@gmail.com>
2017-05-16 23:52:31 +0200

    PHPDOC

--- src/AppBundle/Service/PostService.php
+++ src/AppBundle/Service/PostService.php
@@ -47,6 +47,12 @@ class PostService
         return $post;
     }
 
+    /**
+    *   Retourne les posts de la page
+    *
+    *   @param int $page
+    *   @return Paginator $pag
+    */
     public function getPostsByPage($page)
     {
         $qb = $this->postRepository->createQueryBuilder('post');
